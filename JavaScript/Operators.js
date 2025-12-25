168a804dd3d7e428b2318cc18dfac2d14152643b
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-12-25 10:21:19 +0530

    Add files via upload

---
+++ JavaScript/Operators.js
@@ -0,0 +1,89 @@
+// let a = 12;
+// let b = 5;
+
+// console.log(a + b); //Adition
+// console.log(a - b); // Subtraction 
+// console.log(a * b); // Multiplication
+// console.log(a / b); // Division
+// console.log(a % b); // Modulus
+// console.log(a ** b); // Exponentiation
+
+// let a = 10;
+// console.log(a+1)  //Increment....Adds 1 to a number (++)
+// console.log(a-1)  //Decrement....Subtracts 1 from a number (--)
+// a++;
+// console.log(a)
+// a--;
+// console.log(a)
+
+// console.log(a++); // prints 10, then a becomes 11 //Post Increment
+// console.log(a)
+// console.log(a--); //Post Decrement
+// console.log(a)
+
+// console.log(++a); // a becomes 11, then prints 11 //Pre Increment
+// console.log(--a); // Pre Decrement
+
+// let x = 5;
+// console.log(x++); // 5
+// console.log(x--); // 4 //5
+// console.log(x)
+
+// let a = 5;
+// console.log(a++);
+// console.log(--a);
+
+//Unary Plus Operator to convert a value to a number
+// let a = "23";
+// console.log(typeof a)
+// let b = +a;
+// console.log(b)
+// console.log(typeof b)
+// console.log(+true)
+// console.log(+false)
+
+
+// //Assignment Operators
+// let a = 10;
+// // a = a + 5;
+// console.log(a += 5) // a = a + 5
+// console.log(a -= 5) // a = a - 5
+// console.log( a *= 5) // a = a * 5
+// console.log( a /= 2) // a = a / 2
+// console.log( a %= 4) // a = a % 4
+// console.log( a **= 2) // a = a ** 2
+
+
+// Comparison Operators
+// let a = 10;
+// let b = 20;
+// let c = "10";
+// console.log(a == c)
+// console.log(a === c)
+// console.log(a != b)
+// console.log(a != c)
+// console.log(a !== c)
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a >= b)
+// console.log(a <= b)
+// console.log(a == b)
+
+// Type Coercion
+// let a = "5";
+// let b = 2;
+
+// console.log(a + b)
+// console.log(a - b)
+
+// console.log(true + 1)
+
+
+// Logical Operators
+
+let age = 25;
+let marks = 90;
+
+// console.log(age>20 && marks<70)
+// console.log(age>30 || marks<70)
+console.log(!(age>30))
