02528174e0772f6ed7f4f70c9f860f009a1efb2b
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-12-25 10:23:33 +0530

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,43 @@
+// var age;  //Declaration
+// age = 24;  //Initialization
+
+// var age = 24; //Declaration + Intialization
+
+// let age = 23; 
+
+// const age= 34;
+
+// var age = 10;
+// // console.log(age)
+// var age; //Can Redeclare and update
+// age = 29;
+// console.log(age)
+
+// let age = 19;
+// // console.log(age)
+// // let age; //can't Redeclare
+// age = 20;  //can update
+// console.log(age)
+
+// const age = 20;
+// // const age; //Can't Redeclare
+// age = 30; //Can't update
+// console.log(age)
+
+
+// variables/Identifiers name rules
+
+// let name;
+// let age2;
+// let _temp;
+// let $price;
+// let NAME;
+
+// let 1name;
+
+// let age = 10;    
+// let Age = 20;
+// console.log(age)
+// console.log(Age)
+
+// let class = 20;
