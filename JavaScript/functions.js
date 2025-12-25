d4686ef8275ca9aa15a9177ef992c1f58903f811
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-12-25 10:22:51 +0530

    Add files via upload

---
+++ JavaScript/functions.js
@@ -0,0 +1,91 @@
+//Function Definition or Function Declaration
+
+// function functionName(){
+//     //Code to execute
+// }
+
+
+// function greet(){
+//     console.log("Hello and Welcome")
+//     console.log("Welcome to this session")
+// }
+
+// //Function Call or Invocation
+// greet();
+// // dlfjdlkfj
+// // dfldjflkdjf
+// // dlfjdlkfjdl
+// // ldfjdlkfj
+// // ldfjldkfjdlk
+// // dlfjldkfjldkj
+// greet();
+// greet();
+
+
+// function greet(){
+//     for(let i=1; i<=10; i++){
+//         console.log("Hello and Welcome")
+//     }
+// }
+
+// greet();
+// console.log("okay")
+// console.log("okay")
+// console.log("okay")
+// console.log("okay")
+// console.log("okay")
+// greet();
+
+//Function Parameters
+// function greet(fName){
+//     console.log("Welcome ", fName)
+// }
+
+// greet("Pooja")
+
+// function add(a, b){
+//     console.log("The sum of the numbers is: ", a+b)
+// }
+
+// add(12, 10)
+
+
+// function addition(a, b){
+//     return a+b;
+// }
+
+//  console.log(addition(10, 20))
+// let result = addition(20, 20)
+// console.log(result)
+
+// function number(n){
+//     console.log("Before Return Statement")
+//     return (n**2)
+//     console.log("After return statement")
+// }
+// console.log(number(4))
+// console.log(n)
+
+// Anonymous functions
+
+// let greet = function(fName){
+//     console.log("Hello and Welcome", fName)
+// }
+// greet("Sushma")
+
+//Arrow Function
+// let greet = ()=>{
+//     console.log("Hello and Welcome")
+// }
+
+// greet()
+
+// const greet = (name)=>{
+//     return "Welcome " + name  //function body
+// }
+
+// console.log(greet("Birendra"))
+
+//simplifying the syntax further
+// let square = a => a**2
+// console.log(square(2))
