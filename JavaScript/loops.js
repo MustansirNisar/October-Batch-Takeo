cc058be7de127b771175b2926ccf70e40b240163
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-12-25 10:22:12 +0530

    Add files via upload

---
+++ JavaScript/loops.js
@@ -0,0 +1,61 @@
+//for loop
+// for(initialization; Condition; increment/updation ){
+//     // code to run
+// }
+
+// for (let i = 1; i <= 10; i++){
+//     console.log("Hello and welcome")
+// }
+
+// for(let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+//Calculate the sum of first 10 numbers
+// let sum = 0;
+// for(let i=1; i<=10; i++){
+//     sum += i;
+// }
+// console.log("The sum of the first 10 numbers is: ", sum)
+
+//While Loop
+// Syntax
+// while(condition){
+//     //code to run
+// }
+
+// let i = 1;
+// while(i<=10){
+//     console.log("Hello and Welcome")
+//     i++;
+// }
+
+
+// do...while loop
+// Syntax
+
+// do{
+// } while(condition)
+
+// let i = 1;
+// do{
+//     console.log("Hello and Welcome")
+//     i++;
+// }while(i<=10);
+
+
+// let i = 6;
+// do{
+//     console.log("Hello and Welcome")
+//     i++;
+// }while(i<=5);
+
+//NaN
+// let a = "something"
+// console.log(typeof (a/2) ) 
+
+//prompt()
+
+// let age = prompt("Enter your age: ");
+// console.log("You have entered: ", age)
+// console.log(typeof age)
