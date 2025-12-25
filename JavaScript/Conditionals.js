a663b5e6f3701a077f07b59f69cd2bb82c7969fc
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-12-25 10:21:52 +0530

    Add files via upload

---
+++ JavaScript/Conditionals.js
@@ -0,0 +1,47 @@
+// if (condition){
+//     // code to be executed if the condition is true
+// }
+
+// let age = 10;
+// if (age > 18){
+//     console.log("The user is an adult")
+// }
+
+// if(age<18){
+//     console.log("You are a child")
+// }
+
+// let age = 10;
+// if (age>18){
+//     console.log("You can vote")
+// }
+
+// else{
+//     console.log("You can't vote")
+// }
+
+// let marks = 85;
+// if(marks>=90){
+//     console.log("Grade: A")
+// }
+// else if (marks >=80){
+//     console.log("Grade: B")
+// }
+// else if (marks>=70){
+//     console.log("Grade: C")
+// }
+// else{
+//     console.log("Not Qualified")
+// }
+
+
+
+//Ternary Operator //compact/shorthand way of writing the if...else
+// condition? value_if_true: value_if_false
+// let age = 19;
+// age>18?console.log("You can vote"):console.log("You can't vote")
+
+// let a = 2;
+// a>12?console.log("true"):
+// a===10?console.log("a is exactly 10"):
+// a===2?console.log("a is exactly 2"): console.log("Nothing")
