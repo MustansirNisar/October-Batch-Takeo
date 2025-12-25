0e5d1b5b5b465101bfd15b2d34349858a5633d00
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2025-12-25 10:23:51 +0530

    Add files via upload

---
+++ JavaScript/strings.js
@@ -0,0 +1,75 @@
+// let fName = "Pooja"
+// let Name = 'Sushma'
+// console.log(typeof Name)
+// console.log(typeof fName) 
+
+//Length property
+// let fName = "Pooja"
+// console.log(fName.length)
+
+//Accessing values of a string
+// let fName = "Pooja"
+// console.log(fName[0])
+// console.log(fName[1])
+// console.log(fName[2])
+// console.log(fName[3])
+// console.log(fName[4])
+
+//String Methods
+//toUpperCase and toLowerCase
+// let greet = "Welcome"
+// let nString = greet.toUpperCase()
+// console.log(nString)
+// console.log(greet.toUpperCase())
+// console.log(greet.toLowerCase())
+
+//trim()...removes the whitespaces from both ends
+// let greet = "   Welcome   "
+// console.log(greet.length)
+// let nString = greet.trim()
+// console.log(nString.length)
+
+// slice()...extract a portion of the string
+
+// let greet = "Welcome"
+// console.log(greet.slice(0,4))
+// console.log(greet.slice(-4, -2))
+
+//substring()
+// homework
+
+//concat()....joins two or more strings together
+// let greet = "Welcome"
+// let fName = "Pooja"
+// let nString = greet.concat(" ", fName)
+// console.log(nString)
+// console.log(greet.concat(" ",fName))
+
+// replace()...replces a value
+// let text = "I like cats because cats are beautiful"
+// console.log(text.replace("cats", "dogs"))
+// console.log(text.replaceAll("cats", "dogs"))
+
+
+//indexof().....returns the position of the first occurence
+
+// let text = "JavaScript is fun"
+// console.log(text.indexOf("Scr"))
+// console.log(text.indexOf("Java"))
+
+
+// charAt()...returns a specific character
+// let word = "Python"
+// console.log(word.charAt(4))
+
+//Tempelate Literals
+// let fName = "Sushma"
+// console.log(`Hello, ${fName}, How are you doing?`)
+// let a = 6;
+// let b = 10;
+// console.log(`The sum of the numbers is ${a+b}`)
+
+// let text = "Roses are red \n violets are blue"
+let text = `Roses are red
+violets are blue`;
+console.log(text)
