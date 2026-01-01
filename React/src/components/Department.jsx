d69e2915f829d80993b0a008822c9bf7f6be5cec
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-01 17:22:43 +0530

    Add files via upload

---
+++ React/src/components/Department.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Student from './Student'
+
+function Department() {
+  return (
+    <div style={{backgroundColor: 'green', border: '2px solid black', padding: "20px"}}>
+      <h1>Department</h1>
+      <Student/>
+    </div>
+  )
+}
+
+export default Department
