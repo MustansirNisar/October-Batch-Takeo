47fb235ec9fc149a2f3d6afb3738973a8ef198dd
Mustansir Nisar <140247214+MustansirNisar@users.noreply.github.com>
2026-01-02 22:43:18 +0530

    Add files via upload

---
+++ React/src/components/Learner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Learner() {
+  return (
+    <div>
+      <h1>Sushma</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, vero!</p>
+    </div>
+  )
+}
+
+export default Learner
